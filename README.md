cdd5c468aa3e5c481d6a1d953c327c2c18f1bfec
mayeut <mayeut@users.noreply.github.com>
2020-04-25 13:31:53 +0200

    Update OpenSSL from 1.0.2u to 1.1.1h, drop python 3.4 support
    Drop python 3.4 support, it does not support OpenSSL 1.1.1
    Update OpenSSL from 1.0.2u to 1.1.1h, requires patching CPython ssl tests with files from CPython 3.5.10
    Bundle certificates from certifi in the patch

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
 patch-macos-python-openssl [![badge-build]][link-build]
 =======================================================
-Create _ssl module patch with updated OpenSSL for macOS Python 3.4.4 & 3.5.4 **official installers**
+Create _ssl module patch with updated OpenSSL for macOS Python 3.5.4 **official installers**
 
 Why a patch ?
 -------------
-macOS python 3.4.4 & 3.5.4 official installers are using Apple provided OpenSSL 0.9.8 which does not support TLS v1.1/v1.2
+macOS python 3.5.4 official installers are using Apple provided OpenSSL 0.9.8 which does not support TLS v1.1/v1.2
 
 What is the patch ?
 -------------------
-The patch provides OpenSSL 1.0.2u and an _ssl module rebuilt using this updated OpenSSL
+The patch provides OpenSSL 1.1.1h and an _ssl module rebuilt using this updated OpenSSL
 
 How to install the patch ?
 --------------------------
-For Python 3.4.4:
-```
-curl -fsSLO https://github.com/mayeut/patch-macos-python-openssl/releases/download/v1.0.2u/patch-macos-python-3.4-openssl-v1.0.2u.tar.gz
-sudo tar -C /Library/Frameworks/Python.framework/Versions/3.4/ -xmf patch-macos-python-3.4-openssl-v1.0.2u.tar.gz
-```
 For Python 3.5.4:
 ```
-curl -fsSLO https://github.com/mayeut/patch-macos-python-openssl/releases/download/v1.0.2u/patch-macos-python-3.5-openssl-v1.0.2u.tar.gz
-sudo tar -C /Library/Frameworks/Python.framework/Versions/3.5/ -xmf patch-macos-python-3.5-openssl-v1.0.2u.tar.gz
+curl -fsSLO https://github.com/mayeut/patch-macos-python-openssl/releases/download/v1.1.1g/patch-macos-python-3.5-openssl-v1.1.1h.tar.gz
+sudo tar -C /Library/Frameworks/Python.framework/Versions/3.5/ -xmf patch-macos-python-3.5-openssl-v1.1.1h.tar.gz
 ```
 
 [badge-build]: https://travis-ci.org/mayeut/patch-macos-python-openssl.svg?branch=master "Build Status"
--- create-patch.sh
+++ create-patch.sh
@@ -2,14 +2,10 @@
 
 set -x -e -u
 
-OPENSSL_VERSION=1.0.2u
-OPENSSL_SHA256=ecd0c6ffb493dd06707d38b14bb4d8c2288bb7033735606569d8f90f89669d16
+OPENSSL_VERSION=1.1.1h
+OPENSSL_SHA256=5c9ca8774bd7b03e5784f26ae9e9e6d749c9da2438545077e6b3d755a06595d9
 
-if [ "${PYTHON_VERSION}" == "3.4" ]; then
-	PYTHON_VERSION_FULL=3.4.4
-	PYTHON_SOURCE_SHA256=bc93e944025816ec360712b4c42d8d5f729eaed2b26585e9bc8844f93f0c382e
-	PYTHON_PKG_SHA256=a7ed0d70989db872a229a988bd7376a54858f43abbf70fa95cbecfebe8cb5544
-elif [ "${PYTHON_VERSION}" == "3.5" ]; then
+if [ "${PYTHON_VERSION}" == "3.5" ]; then
 	PYTHON_VERSION_FULL=3.5.4
 	PYTHON_SOURCE_SHA256=6ed87a8b6c758cc3299a8b433e8a9a9122054ad5bc8aad43299cff3a53d8ca44
 	PYTHON_PKG_SHA256=6209c690925c0826fa767b7c9581b8ebe556160e1eac7a0dbd13500870a097a2
@@ -36,32 +32,39 @@ test "${SHA256%% *}" = "${OPENSSL_SHA256}"
 # install python
 if [ "${TRAVIS:-}" == "true" ]; then
 	sudo installer -pkg python-$PYTHON_VERSION_FULL-macosx10.6.pkg -target /
+	sudo patch -u -i $(pwd)/test_ssl.py.patch -d ${PYTHON_PREFIX}/lib/python${PYTHON_VERSION}/test
+	sudo curl -fsSLo ${PYTHON_PREFIX}/lib/python${PYTHON_VERSION}/test/keycert.pem https://raw.githubusercontent.com/python/cpython/v3.5.10/Lib/test/keycert.pem
+	sudo curl -fsSLo ${PYTHON_PREFIX}/lib/python${PYTHON_VERSION}/test/ffdh3072.pem https://raw.githubusercontent.com/python/cpython/v3.5.10/Lib/test/ffdh3072.pem
 fi
+
+# install certifi
+curl -fsSL https://bootstrap.pypa.io/get-pip.py | $PYTHON_PREFIX/bin/python$PYTHON_VERSION
+$PYTHON_PREFIX/bin/python$PYTHON_VERSION -m pip install -U certifi
 
 # let's build OpenSSL
 tar -xf openssl-$OPENSSL_VERSION.tar.gz
 pushd openssl-$OPENSSL_VERSION
-	OPENSSL_CONF="no-krb5 no-idea no-mdc2 no-rc5 no-zlib enable-tlsext no-ssl2 no-ssl3 shared --prefix=$PYTHON_PREFIX --openssldir=$PYTHON_PREFIX/etc/openssl"
-	./Configure darwin-i386-cc --install_prefix=$INSTALL_DIR/i386 $OPENSSL_CONF &> /dev/null
+	OPENSSL_CONF="no-idea no-mdc2 no-rc5 no-zlib no-ssl2 no-ssl3 shared --prefix=$PYTHON_PREFIX --openssldir=$PYTHON_PREFIX/etc/openssl"
+	./Configure darwin-i386-cc $OPENSSL_CONF &> /dev/null
 	make depend &>/dev/null
 	make all &>/dev/null
 	make test &> /dev/null
-	make install_sw &>/dev/null
+	make DESTDIR=$INSTALL_DIR/i386 install_sw &>/dev/null
 	make clean &>/dev/null
 
-	./Configure darwin64-x86_64-cc enable-ec_nistp_64_gcc_128 --install_prefix=$INSTALL_DIR/x86_64 $OPENSSL_CONF &> /dev/null
+	./Configure darwin64-x86_64-cc enable-ec_nistp_64_gcc_128 $OPENSSL_CONF &> /dev/null
 	make depend &>/dev/null
 	make all &>/dev/null
 	make test &> /dev/null
-	make install_sw &>/dev/null
+	make DESTDIR=$INSTALL_DIR/x86_64 install_sw &>/dev/null
 	make clean &>/dev/null
 
 	mkdir -p $INSTALL_DIR/$PYTHON_PREFIX/lib
-	lipo -create $INSTALL_DIR/i386/$PYTHON_PREFIX/lib/libcrypto.1.0.0.dylib $INSTALL_DIR/x86_64/$PYTHON_PREFIX/lib/libcrypto.1.0.0.dylib -output $INSTALL_DIR/$PYTHON_PREFIX/lib/libcrypto.1.0.0.dylib
-	lipo -create $INSTALL_DIR/i386/$PYTHON_PREFIX/lib/libssl.1.0.0.dylib $INSTALL_DIR/x86_64/$PYTHON_PREFIX/lib/libssl.1.0.0.dylib -output $INSTALL_DIR/$PYTHON_PREFIX/lib/libssl.1.0.0.dylib
+	lipo -create $INSTALL_DIR/i386/$PYTHON_PREFIX/lib/libcrypto.1.1.dylib $INSTALL_DIR/x86_64/$PYTHON_PREFIX/lib/libcrypto.1.1.dylib -output $INSTALL_DIR/$PYTHON_PREFIX/lib/libcrypto.1.1.dylib
+	lipo -create $INSTALL_DIR/i386/$PYTHON_PREFIX/lib/libssl.1.1.dylib $INSTALL_DIR/x86_64/$PYTHON_PREFIX/lib/libssl.1.1.dylib -output $INSTALL_DIR/$PYTHON_PREFIX/lib/libssl.1.1.dylib
 	pushd $INSTALL_DIR/$PYTHON_PREFIX/lib
-		ln -s libcrypto.1.0.0.dylib libcrypto.dylib
-		ln -s libssl.1.0.0.dylib libssl.dylib
+		ln -s libcrypto.1.1.dylib libcrypto.dylib
+		ln -s libssl.1.1.dylib libssl.dylib
 	popd
 popd
 
@@ -89,10 +92,13 @@ popd
 PATCH_SUFFIX=$(git describe --dirty 2>/dev/null || echo "local")
 PATCH_NAME=patch-macos-python-$PYTHON_VERSION-openssl-$PATCH_SUFFIX.tar.gz
 pushd $INSTALL_DIR/$PYTHON_PREFIX
+	# copy certificates
+	mkdir -p etc/openssl
+	cp -f $($PYTHON_PREFIX/bin/python$PYTHON_VERSION -m certifi) etc/openssl/cert.pem
 	# update rights
-	sudo chown -R root:admin lib
+	sudo chown -R root:admin lib etc
 	# create tar
-	tar -czf $INSTALL_DIR/../$PATCH_NAME lib
+	tar -czf $INSTALL_DIR/../$PATCH_NAME lib etc
 popd
 
 # apply patch
@@ -104,5 +110,7 @@ PY64_OPENSSL_VERSION=$($PYTHON_PREFIX/bin/python$PYTHON_VERSION -c "import ssl; 
 PY32_OPENSSL_VERSION=$($PYTHON_PREFIX/bin/python$PYTHON_VERSION-32 -c "import ssl; print(ssl.OPENSSL_VERSION)")
 test "$OPENSSL_VERSION" = "$PY64_OPENSSL_VERSION"
 test "$OPENSSL_VERSION" = "$PY32_OPENSSL_VERSION"
-$PYTHON_PREFIX/bin/python$PYTHON_VERSION -m test test_ssl
-$PYTHON_PREFIX/bin/python$PYTHON_VERSION-32 -m test test_ssl
+$PYTHON_PREFIX/bin/python$PYTHON_VERSION -m test -vvv test_ssl
+$PYTHON_PREFIX/bin/python$PYTHON_VERSION-32 -m test -vvv test_ssl
+$PYTHON_PREFIX/bin/python$PYTHON_VERSION -c "from urllib.request import urlopen; print(urlopen('https://raw.githubusercontent.com/mayeut/patch-macos-python-openssl/master/README.md').read())"
+$PYTHON_PREFIX/bin/python$PYTHON_VERSION-32 -c "from urllib.request import urlopen; print(urlopen('https://raw.githubusercontent.com/mayeut/patch-macos-python-openssl/master/README.md').read())"
